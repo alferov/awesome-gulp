7d70302163095c1e435334beb60dc4a021968cf1
Philipp Alferov <palferov@securesense.ca>
2015-08-25 23:53:55 -0400

    Add deployment to gh-pages task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,8 +4,12 @@ import gulpLoadPlugins from 'gulp-load-plugins';
 const $ = gulpLoadPlugins();
 
 gulp.task('markdown', () => {
-
   return gulp.src('./README.md')
     .pipe($.markdown())
     .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('deploy', () => {
+  return gulp.src('./tmp/**/*')
+    .pipe($.ghPages());
 });
