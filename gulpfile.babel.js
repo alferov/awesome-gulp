8b9635627873390ab20b27a4341474c48b1c5e67
Philipp Alferov <palferov@securesense.ca>
2015-08-26 21:43:16 -0400

    Add gulp-inject and refine the markdown task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,22 @@
+'use strict';
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 
 const $ = gulpLoadPlugins();
 
+function fileContents (filePath, file) {
+  return file.contents.toString();
+}
+
 gulp.task('markdown', () => {
-  return gulp.src('./README.md')
-    .pipe($.markdown())
+  let markdown = gulp.src('./README.md')
+    .pipe($.markdown());
+
+  return gulp
+    .src('./index.html')
+    .pipe($.inject(markdown, {
+      transform: fileContents
+    }))
     .pipe(gulp.dest('./tmp'));
 });
 
