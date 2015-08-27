c1dc32da5636fb6228d244af24cda98560b264dc
Philipp Alferov <philipp.alferov@gmail.com>
2015-08-27 17:07:34 -0400

    Refine task flow

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,31 +1,40 @@
 'use strict';
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
+import del from 'del';
 
 const $ = gulpLoadPlugins();
 
-function fileContents (filePath, file) {
+function fileContents(filePath, file) {
   return file.contents.toString();
 }
 
 /*
-* Inject compiled markdown to index.html
-*/
-gulp.task('markdown', () => {
+ * Inject compiled markdown to index.html
+ */
+gulp.task('markdown', ['clean'], () => {
   let markdown = gulp.src('./README.md')
     .pipe($.markdown());
 
-  return gulp
-    .src('./index.html')
+  let styles = gulp.src('./gh-pages/*.css')
+    .pipe(gulp.dest('./tmp'));
+
+  return gulp.src('./gh-pages/index.html')
     .pipe($.inject(markdown, {
       transform: fileContents
+    }))
+    .pipe($.inject(styles, {
+      read: false,
+      relative: true
     }))
     .pipe(gulp.dest('./tmp'));
 });
 
+gulp.task('clean', del.bind(null, ['./tmp']));
+
 /*
-* Publish to gh-pages
-*/
+ * Publish to gh-pages
+ */
 gulp.task('deploy', () => {
   return gulp.src('./tmp/**/*')
     .pipe($.ghPages());
