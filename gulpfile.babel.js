a687f34163ae434fcdead412b4edb34813b5930f
Philipp Alferov <philipp.alferov@gmail.com>
2015-08-27 20:20:38 -0400

    Integrate browser-sync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import del from 'del';
+import browserSync from 'browser-sync';
 
 const $ = gulpLoadPlugins();
 const config = {
@@ -17,18 +18,16 @@ const config = {
   }
 };
 
-/*
-* Helpers
-*/
-
+/**
+ * Helpers
+ */
 function fileContents(filePath, file) {
   return file.contents.toString();
 }
 
-/*
-* Inject compiled markdown to index.html
-*/
-
+/**
+ * Inject compiled markdown to index.html
+ */
 gulp.task('markdown', ['clean'], () => {
   let markdown = gulp.src(config.src.markdown)
     .pipe($.markdown());
@@ -36,7 +35,7 @@ gulp.task('markdown', ['clean'], () => {
   let styles = gulp.src(config.src.styles)
     .pipe(gulp.dest(config.dist));
 
-  return gulp.src(config.src.styles.html)
+  return gulp.src(config.src.html)
     .pipe($.inject(markdown, {
       transform: fileContents
     }))
@@ -44,16 +43,38 @@ gulp.task('markdown', ['clean'], () => {
       read: false,
       relative: true
     }))
-    .pipe(gulp.dest(config.dist));
+    .pipe(gulp.dest(config.dist))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('clean', del.bind(null, [config.dist]));
 
-/*
-* Publish to gh-pages
-*/
+gulp.task('serve', function() {
+  browserSync({
+    port: config.browserSync.port,
+    server: {
+      baseDir: config.browserSync.baseDir,
+    },
+    logConnections: true,
+    logFileChanges: true,
+    notify: true
+  });
+});
 
+gulp.task('watch', function() {
+  gulp.watch([
+    config.src.styles,
+    config.src.markdown,
+    config.src.html
+  ], ['markdown']);
+});
+
+/**
+ * Publish to gh-pages
+ */
 gulp.task('deploy', () => {
   return gulp.src('./tmp/**/*')
     .pipe($.ghPages());
 });
+
+gulp.task('default', ['markdown', 'serve', 'watch']);
